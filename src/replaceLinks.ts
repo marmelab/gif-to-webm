a5bb6bb9bf6a4c50449b779af643e7dadcb545bb
Jean-Baptiste Kaiser <jb@marmelab.com>
2023-04-05 12:37:54 +0200

    fix file replacement

--- src/replaceLinks.ts
+++ src/replaceLinks.ts
@@ -22,30 +22,24 @@ export const replaceLinks = async (
   const docFiles = await findDocFiles(sourceFolder, pattern);
   const gifFileNames = gifFiles.map((file) => file.split("/").pop());
   const templateAsString = fs.readFileSync(template, "utf8");
-  const promises = gifFileNames.filter(Boolean).map((gifFileName) => {
-    const options: ReplaceInFileConfig = {
-      files: docFiles,
-      from: RegExp(getRegExp(gifFileName!), "g"),
-      to: (match) => {
-        const previousLink = match.match(RegExp(getRegExp(gifFileName!)))?.[1];
-        return templateAsString.replace(
-          "%webm_file%",
-          `${previousLink?.split(".").slice(0, -1).join(".")}.webm`
-        );
-      },
-    };
-    return replaceInFile(options).then((results) =>
-      results.filter((file) => file.hasChanged).map((file) => file.file)
-    );
-  });
-  // We need to process the replacement promises one by one otherwise it can create concurrent
-  // editions of the same file which can lead to corrupted files.
-  const allChangedFiles: string[] = [];
-  for (const promise of promises) {
-    const changedFiles = await promise;
-    allChangedFiles.push(
-      ...changedFiles.filter((file) => !allChangedFiles.includes(file))
-    );
-  }
-  return allChangedFiles;
+  const from = gifFileNames
+    .filter(Boolean)
+    .map((gifFileName) => RegExp(getRegExp(gifFileName!), "g"));
+  const to = gifFileNames
+    .filter(Boolean)
+    .map((gifFileName) => (match: string) => {
+      const previousLink = match.match(RegExp(getRegExp(gifFileName!)))?.[1];
+      return templateAsString.replace(
+        "%webm_file%",
+        `${previousLink?.split(".").slice(0, -1).join(".")}.webm`
+      );
+    });
+  const options: ReplaceInFileConfig = {
+    files: docFiles,
+    from,
+    to,
+  };
+  return replaceInFile(options).then((results) =>
+    results.filter((file) => file.hasChanged).map((file) => file.file)
+  );
 };
